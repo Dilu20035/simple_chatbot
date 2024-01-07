8e4314e92b2db7637f42956465131b6884a1b076
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-01-07 16:07:48 +0530

    Update MEDICAL-CHATBOT.py

--- MEDICAL-CHATBOT.py
+++ MEDICAL-CHATBOT.py
@@ -122,7 +122,7 @@ with st.sidebar:
 
 st.title('')
 # Streamlit App Title
-st.markdown("<h1 style='text-align: center;'>Medical Diagnostic Chatbot</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Medical Diagnostic AI-Chatbot</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>Ask Medical Related Queries With Our Chatbot And Find The Results</p>", unsafe_allow_html=True)
 st.markdown("")
 
