683e65c5c4bbae5c6d428883fc439360bfe9e68a
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-01-07 08:06:13 +0530

    Update MEDICAL-CHATBOT.py

--- MEDICAL-CHATBOT.py
+++ MEDICAL-CHATBOT.py
@@ -136,15 +136,6 @@ def get_openai_response(user_input):
     )
     return response.choices[0].text.strip()
 
-# Display conversation history
-for message in st.session_state.conversation:
-    if message["role"] == "user":
-        st.text_input("You:", value=message["content"], key=message["content"], disabled=True)
-    elif message["role"] == "bot":
-        st.text_area("Bot:", value=message["content"], key=message["content"], disabled=True)
-
-# Create a text input box for user input
-user_input = st.text_input("You:", "")
 
 # Ask button to trigger the conversation
 if st.button("Ask"):
@@ -159,3 +150,14 @@ if st.button("Ask"):
         st.text_area("Bot:", value=bot_response, key=bot_response, disabled=True)
     else:
         st.warning("Please enter a question.")
+
+# Display conversation history
+for message in st.session_state.conversation:
+    if message["role"] == "user":
+        st.text_input("You:", value=message["content"], key=message["content"], disabled=True)
+    elif message["role"] == "bot":
+        st.text_area("Bot:", value=message["content"], key=message["content"], disabled=True)
+
+# Create a text input box for user input
+user_input = st.text_input("You:", "")
+
